fb8d59949653aae8c5b271f7b483b6676698ec71
Omri93h <omri93h@gmail.com>
2023-06-27 21:26:19 +0300

    added Battery.js

---
+++ src/screens/MyCar/Battery.js
@@ -0,0 +1,194 @@
+import React from 'react';
+import {TextInput as MaterialTextInput} from '@react-native-material/core';
+import {
+  View,
+  Text,
+  Modal,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
+
+const Battery = ({modalVisible, setModalVisible}) => {
+  const [notesInputValue, setNotesInputValue] = React.useState('');
+  const [locationInputValue, setLocationInputValue] = React.useState('');
+  const [imageUris, setImageUris] = React.useState([]);
+
+  const handleUploadImage = () => {
+    // Simulating image upload by generating a random image URL
+    const randomImageUrl = `https://picsum.photos/200/300?random=${Math.random()}`;
+    setImageUris([...imageUris, randomImageUrl]);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+    setNotesInputValue('');
+    setLocationInputValue('');
+    setImageUris([]);
+  };
+
+  return (
+    <Modal
+      visible={modalVisible}
+      animationType="slide"
+      onRequestClose={closeModal}>
+      <View style={styles.modalWrapper}>
+        <View style={styles.modalContainer}>
+          <View style={styles.blackBackground} />
+
+          <View style={styles.header}>
+            <TouchableOpacity onPress={closeModal}>
+              <Image
+                source={require('./../../assets/icons/arrow-left.png')}
+                style={styles.arrowIcon}
+              />
+            </TouchableOpacity>
+            <Text style={styles.headerText}>Battery</Text>
+            <TouchableOpacity onPress={closeModal}>
+              <Image
+                source={require('./../../assets/icons/arrow-right.png')}
+                style={styles.arrowIcon}
+              />
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.content}>
+            <View style={styles.BatteryIconContainer}>
+              <Image
+                source={require('./../../assets/icons/battery.png')}
+                style={styles.batteryIcon}
+              />
+            </View>
+            <Text style={styles.detailsHeader}>Details</Text>
+            <TextInput
+              multiline
+              placeholder="Enter Notes ..."
+              value={notesInputValue}
+              onChangeText={setNotesInputValue}
+              style={styles.textArea}
+            />
+
+            <TouchableOpacity
+              onPress={handleUploadImage}
+              style={styles.uploadButton}>
+              <Text style={styles.buttonText}>Upload Images</Text>
+            </TouchableOpacity>
+
+            <ScrollView horizontal={true} style={styles.imageContainer}>
+              {imageUris.map((uri, index) => (
+                <View key={index} style={styles.imageCard}>
+                  <Image source={{uri}} style={styles.image} />
+                </View>
+              ))}
+            </ScrollView>
+
+            <MaterialTextInput
+              variant="outlined"
+              value={locationInputValue}
+              label="* Current location & directions"
+              inputContainerStyle={styles.locationTextArea}
+              onChangeText={setLocationInputValue}
+            />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const windowsHight = Dimensions.get('window').height;
+const styles = StyleSheet.create({
+  modalWrapper: {
+    flex: 1,
+    marginTop: 22,
+  },
+  modalContainer: {
+    flex: 1,
+  },
+  blackBackground: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    height: '50%',
+    backgroundColor: 'black',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'black',
+    paddingVertical: 16,
+    paddingHorizontal: 24,
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white',
+  },
+  arrowIcon: {
+    width: 36,
+    height: 36,
+  },
+  BatteryIconContainer: {
+    alignItems: 'center',
+  },
+  batteryIcon: {
+    width: 90,
+    height: 90,
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    backgroundColor: 'white',
+  },
+  detailsHeader: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  textArea: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 8,
+    height: 100,
+    marginBottom: 16,
+  },
+  locationTextArea: {
+    minHeight: 100,
+    flex: 1,
+    flexWrap: 'wrap',
+  },
+  uploadButton: {
+    backgroundColor: 'blue',
+    padding: 12,
+    marginTop: 16,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  imageContainer: {
+    marginTop: 16,
+    flexDirection: 'row',
+    maxHeight: 120,
+    marginBottom: 16,
+  },
+  imageCard: {
+    marginRight: 8,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 4,
+  },
+});
+
+export default Battery;
--- src/screens/MyCar/HomeScreen.js
+++ src/screens/MyCar/HomeScreen.js
@@ -1,15 +1,95 @@
-import React from 'react';
-import {View, Button} from 'react-native';
-const HomeScreen=({ navigation })=> {
-    
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-          onPress={() => navigation.navigate('Notifications')}
-          title="Go to notifications"
+import React, {useState} from 'react';
+import Battery from './Battery';
+
+import {Alert, Modal,Button, StyleSheet, Text, Pressable, View} from 'react-native';
+
+const HomeScreen = ({navigation}) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+
+      {/* Modal */}
+      
+      {/* <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal> */}
+      <Button title="Open Modal" onPress={() => setModalVisible(true)} />
+
+      {modalVisible && (
+        <Battery
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
         />
-      </View>
-    );
-  }
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
+
+
 
 export default HomeScreen;
