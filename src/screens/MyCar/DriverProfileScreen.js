5f10d8c5689b7a880652051b872210d70e015655
Sameer Zaher <sameerzaher@gmail.com>
2023-06-30 17:38:31 +0300

    added scrolable screen

--- src/screens/MyCar/DriverProfileScreen.js
+++ src/screens/MyCar/DriverProfileScreen.js
@@ -1,46 +1,80 @@
-import React, {useState} from 'react';
-import {View, Button, Pressable, StyleSheet, Text} from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Pressable, StyleSheet, Text, ScrollView } from 'react-native';
 import ModalPopup from './components/ModalPopup';
 import DocumentField from './components/DocumentField';
 import InputField from './components/InputField';
-const DriverProfileScreen=({ navigation })=> {
 
+const DriverProfileScreen = ({ navigation }) => {
   const [modalVisible, setModalVisible] = useState(false);
   const openModal = () => {
     setModalVisible(true);
   };
-
   const closeModal = () => {
     setModalVisible(false);
   };
 
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-          onPress={() => navigation.navigate('Home')}
-          title="Go to Home"
-        />
-      <Text style={styles.textStyle}>פרטים כלליים</Text>
-      <InputField label ="שם פרטי" placeholder="sameer"/>
-      <InputField label ="שם משפחה" placeholder="sameer"/>
-      <InputField label ="מספר עובד" placeholder="sameer"/>
-      <InputField label ="תאריך לידה" placeholder="sameer"/>
-      <Text style={styles.textStyle}>פרטי תקשורת</Text>
-      <InputField label ="כתובת מייל" placeholder="sameer"/>
-      <InputField label ="טלפון" placeholder="sameer"/> 
-      <Text style={styles.textStyle}>פרטי רישיון נהיגה</Text>     
-      <InputField label ="מספר רישיון נהיגה" placeholder="sameer"/>
-      <InputField label ="תוקף רישיון נהיגה" placeholder="sameer"/>
-     <Text style={styles.textStyle}>מסמסכים</Text>
-     <DocumentField/>
-     <ModalPopup visible={modalVisible} onClose={closeModal} />
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.header}>
+        <Button onPress={() => navigation.navigate('Home')} title="Go to Home" />
+      </View>
+      <View style={styles.mainContainer}>
+        <Text style={styles.textStyle}>פרטים כלליים</Text>
+        <View style={styles.inputContainer}>
+          <InputField label="שם פרטי" placeholder="אביב" />
+          <InputField label="שם משפחה" placeholder="שרון" />
+          <InputField label="מספר עובד" placeholder="537 221" />
+          <InputField label="תאריך לידה" placeholder="23.04.1990" />
+        </View>
+        <Text style={styles.textStyle}>פרטי תקשורת</Text>
+        <View style={styles.inputContainer}>
+          <InputField label="כתובת מייל" placeholder="AvivS@gmail.com" />
+          <InputField label="טלפון" placeholder="0547883115" />
+        </View>
+        <Text style={styles.textStyle}>פרטי רישיון נהיגה</Text>
+        <View style={styles.inputContainer}>
+          <InputField label="מספר רישיון נהיגה" placeholder="84277081" />
+          <InputField label="תוקף רישיון נהיגה" placeholder="23.05.2025" />
+        </View>
+
+        <Text style={styles.textStyle}>מסמסכים</Text>
+        <Text>צילום רישיון נהיגה</Text>
+        <DocumentField />
+      </View>
+      <ModalPopup visible={modalVisible} onClose={closeModal} />
       <Pressable style={[styles.button, styles.buttonOpen]} onPress={openModal}>
         <Text style={styles.textStyle}>התנתק</Text>
       </Pressable>
-      </View>
-    );
-  }
+    </ScrollView>
+  );
+};
+
 const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    alignSelf: 'flex-start',
+    marginLeft: 10,
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  mainContainer: {
+    alignItems: 'flex-end',
+    paddingHorizontal: 20,
+    marginBottom: 20,
+  },
+  inputContainer: {
+    marginBottom: 10,
+  },
+  textStyle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'right',
+  },
   button: {
     borderRadius: 20,
     padding: 10,
@@ -52,6 +86,6 @@ const styles = StyleSheet.create({
   buttonClose: {
     backgroundColor: '#2196F3',
   },
-})
-export default DriverProfileScreen;
+});
 
+export default DriverProfileScreen;
