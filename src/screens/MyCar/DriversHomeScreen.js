0393337eff0fac37a9c720d8be4ca16fee75a545
Suf <sufkarmon2@gmail.com>
2023-06-30 17:24:37 +0300

    pretify stuff

--- src/screens/MyCar/DriversHomeScreen.js
+++ src/screens/MyCar/DriversHomeScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { View, Button, StyleSheet, ScrollView, Image } from 'react-native';
+import {View, Button, StyleSheet, ScrollView, Image} from 'react-native';
 
-const DriversHomeScreen = ({ navigation }) => {
+const DriversHomeScreen = ({navigation}) => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.backgroundContainer}>
@@ -21,12 +21,11 @@ const DriversHomeScreen = ({ navigation }) => {
             resizeMode="contain"
           />
         </View>
-        {/* Additional Image */}
-        <View style={styles.additionalImageContainer}>
+        {/* left shape */}
+        <View style={styles.leftShapeContainer}>
           <Image
             source={require('../../assets/B1-assets/backgroundShape1.png')}
-            style={styles.additionalImage}
-//            resizeMode="cover"
+            style={styles.leftShape}
           />
         </View>
         {/* Add your custom content for the Drivers Home Page */}
@@ -64,7 +63,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: '50%',
     left: '50%',
-    transform: [{ translateX: -50 }, { translateY: -50 }],
+    transform: [{translateX: -50}, {translateY: -50}],
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -73,15 +72,15 @@ const styles = StyleSheet.create({
     height: 150,
     opacity: 0.8, // Adjust this opacity as needed
   },
-  additionalImageContainer: {
+  leftShapeContainer: {
     position: 'absolute',
     top: '50%',
     left: 0,
-    transform: [{ translateY: -50 }],
+    transform: [{translateY: -50}],
     alignItems: 'flex-start',
     justifyContent: 'center',
   },
-  additionalImage: {
+  leftShape: {
     width: 125,
     height: 290, // Adjust the height to match the logo's height
     opacity: 0.8, // Adjust this opacity as needed
--- src/screens/MyCar/HomeScreen.js
+++ src/screens/MyCar/HomeScreen.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import {View, Button} from 'react-native';
-const HomeScreen=({ navigation })=> {
-    
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-          onPress={() => navigation.navigate('Notifications')}
-          title="Go to notifications"
-        />
-        <Button
-            onPress={() => navigation.navigate('DriversHomeScreen')}
-            title="Go to Drivers Home Screen"
-        />
-      </View>
-    );
-  }
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+      <Button
+        onPress={() => navigation.navigate('DriversHomeScreen')}
+        title="Go to Drivers Home Screen"
+      />
+    </View>
+  );
+};
 
 export default HomeScreen;
