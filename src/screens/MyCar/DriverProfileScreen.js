9b46ad0b7a0144bd29ea25931f81c3ccb279c730
Sameer Zaher <sameerzaher@gmail.com>
2023-06-30 14:27:27 +0300

    added logout button with popup

--- src/screens/MyCar/DriverProfileScreen.js
+++ src/screens/MyCar/DriverProfileScreen.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import {View, Button} from 'react-native';
+import React, {useState} from 'react';
+import {View, Button, Pressable, StyleSheet, Text} from 'react-native';
+import ModalPopup from './components/ModalPopup';
 const DriverProfileScreen=({ navigation })=> {
-    
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const openModal = () => {
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Button
           onPress={() => navigation.navigate('Home')}
-          title="Go to notifications"
+          title="Go to Home"
         />
+     
+     <ModalPopup visible={modalVisible} onClose={closeModal} />
+      <Pressable style={[styles.button, styles.buttonOpen]} onPress={openModal}>
+        <Text style={styles.textStyle}>התנתק</Text>
+      </Pressable>
       </View>
     );
   }
-
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+})
 export default DriverProfileScreen;
+
