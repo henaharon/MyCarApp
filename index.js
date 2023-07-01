ee11d036ecdaa2c579bb370d7f9af11278240926
Mohamed Omarii <mohamed.44301hotmail.com>
2023-07-01 17:10:00 +0300

    removed App.tsx test, CarDetailsScreen.js is the main page flow.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import App from './src/screens/CarDetails/App';
+import App from './src/screens/MyCar/App';
 
 AppRegistry.registerComponent(appName, () => App);
